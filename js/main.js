d09517eb86b61b51bf03e54bcc9c2c041fee7e1a
nzr_pe <joseluis.von@gmail.com>
2021-08-12 16:18:29 -0500

    arreglos del nav-bar

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+let autor= "Ramon Rodrigues";
+let edad= 35;
+const rutadelservidor = "http://192.168.2.30";     
+
+if (edad<=20) {
+
+        console.log('usted es muy joven');  
+}
+else
+{
+        console.log('Usted ya no es un muchachito');
+
+}
+
+function creararray(usuarios) {
+
+
+        usuarios = ["jose","monica","teresa"];
+        return usuarios;
+    
+}
+
+let lista = creararray();
+console.log(
+
+
+       
+);
+console.log(rutadelservidor);
+
